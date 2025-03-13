0fadb57906a1d1cf3cc02b53b4a81a72f30b5da3
asrar <asrar@qbil.nl>
2025-03-13 16:45:04 +0100

    test: Ensure explorer works

--- tests/site.spec.ts
+++ tests/site.spec.ts
@@ -11,3 +11,14 @@ test("Home page loads", async ({ page }) => {
     page.getByRole("heading", { name: "Malloy model explorer" }),
   ).toBeVisible();
 });
+
+test("Explorer page loads", async ({ page }) => {
+  await page.goto("./");
+  await expect(page.getByText("Loading...")).toBeVisible();
+  // wait for loading spinner
+  await expect(page.getByText("Loading...")).not.toBeVisible({
+    timeout: 15 * 1000,
+  });
+  await page.getByText("no_of_orders").click();
+  await expect(page.getByText("100,000")).toBeVisible({ timeout: 5 * 1000 });
+});
