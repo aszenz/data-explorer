48923237759ca2318b256160c47101c4dd14e5f8
asrar <aszenz@gmail.com>
2025-09-27 00:06:57 +0200

    test: Run on webkit

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,6 +44,10 @@ export default defineConfig({
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
 
     // {
     //   name: 'webkit',
